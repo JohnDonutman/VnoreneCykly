bbc3af36590bae4d18248483bab77926939e40c6
Nyklundamade <master.donutman@Qgmail.com>
2023-10-27 10:57:13 +0200

    Přidejte soubory projektu.

---
+++ VnoreneCykly/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VnoreneCykly
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadejte 1. číslo: ");
+            string a_string = Console.ReadLine();
+            Console.WriteLine("Zadejte 2. číslo: ");
+            string b_string = Console.ReadLine();
+            Console.WriteLine("Zadejte 3. číslo: ");
+            string c_string = Console.ReadLine();
+            Console.WriteLine("Zadejte poslední průchod: ");
+            string pruchod_string = Console.ReadLine();
+
+            int a = int.Parse(a_string);
+            int b = int.Parse(b_string);
+            int c = int.Parse(c_string);
+            int pruchod = int.Parse(pruchod_string);
+
+            int i = 0;
+            int j = 0;
+            int k = 0;
+
+            int vycet = 0;
+
+            for (i = 0; i <= a; i++)
+            {
+                for (j = 0; j <= b; j++)
+                {
+                    for (k = 0; k <= c; k++)
+                    {
+                        vycet++;
+                        Console.WriteLine(vycet);
+                        if (vycet == pruchod)
+                        {
+                            k++;
+                            break;
+                        }
+                    }
+                    if (vycet == pruchod)
+                    {
+                        j++;
+                        break;
+                    }
+                }
+                if (vycet == pruchod)
+                {
+                    i++;
+                    break;
+                }
+            }
+            Console.Write("Poslední průchod byl: " + i);
+            Console.Write(", " + j);
+            Console.WriteLine(", " + k);
+        }
+    }
+}
---
+++ VnoreneCykly/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení se řídí přes následující 
+// sadu atributů. Změnou hodnot těchto atributů se upraví informace
+// přidružené k sestavení.
+[assembly: AssemblyTitle("VnoreneCykly")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VnoreneCykly")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení ComVisible na false způsobí neviditelnost typů v tomto sestavení
+// pro komponenty modelu COM. Pokud potřebujete přístup k typu v tomto sestavení
+// komponenty COM, nastavte atribut ComVisible daného typu na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID se používá pro ID knihovny typů, pokud je tento projekt vystavený pro COM.
+[assembly: Guid("043c950f-f3da-4203-bb76-c89a92c4d5ae")]
+
+// Informace o verzi sestavení se skládá z těchto čtyř hodnot:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny hodnoty nebo nastavit výchozí číslo buildu a revize
+// pomocí zástupného znaku * takto:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
